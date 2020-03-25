7c8fd95b455e71bceef6def6e1680fbae0f13ae9
Arvish <doshi.arvish16@gmail.com>
2020-03-25 22:54:01 +0530

    .

--- BookTime/main/models.py
+++ BookTime/main/models.py
@@ -7,7 +7,8 @@ class Destination(models.Model):
     desc = models.TextField()
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
-'''    
+'''
+    
 # ActiveManager class is created to add extra methods that return
 # filtered QuerySets, we have an 'active' field in the Product model,
 # we add a manager with a filter on that
@@ -15,8 +16,30 @@ class ActiveManager(models.Manager):
     def active(self):
         return self.filter(active=True)
 
+class ProductTag(models.Model):
+    
+    #products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def natural_key(self):
+        return(self.slug,)
+ 
+
+
 
 class Product(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
+    tags = models.ManyToManyField(ProductTag,blank=True)
+   # products = models.ManyToManyField(Product, blank=True)       
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -25,6 +48,26 @@ class Product(models.Model):
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
     objects = ActiveManager() #connecting ActiveManager to model by overriding an attribute called by convention objects
+
+
+# class ProductTag(models.Model):
+    
+#     #products = models.ManyToManyField(Product, blank=True)
+#     name = models.CharField(max_length=32)
+#     slug = models.SlugField(max_length=48)
+#     description = models.TextField(blank=True)
+#     active = models.BooleanField(default=True)
+    
+#     def __str__(self):
+#         return self.name
+    
+#     def natural_key(self):
+#         return(self.slug,)
+    
+    
+
+
+
 '''
 As for any product catalog, having an image for every product is a must.In our case, we 
 want the possibility to have any number of images per product. To accomplish this,
@@ -45,12 +88,6 @@ of categories: one product may have one or more tags, and one tag may contain on
 products
 '''  
 
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=32)
-    slug = models.SlugField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
 
 # "ManyToManyField" automatically creates a linking between two tables, in this case 
 # ProductTag and Products. This linking allows us to create relationships where any tags
